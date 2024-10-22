cd6f344bcbb22c2b7fde79f883a08ba721ba23a0
rohit-2 <rohit4444choudhary@gmail.com>
2024-10-23 00:15:25 +0530

    minor change

--- server/app/routes.py
+++ server/app/routes.py
@@ -4,4 +4,4 @@ main = Blueprint('main', __name__)
 
 @main.route('/')
 def hello():
-    return "Hello Worldsa"
+    return "Hello World.."
--- server/config.py
+++ server/config.py
@@ -2,9 +2,3 @@ class Config:
     DEBUG = False
     TESTING = False
     SECRET_KEY = "nothing"
-
-class ProductionConfig(Config):
-    pass
-
-class DevelopmentConfig(Config):
-    DEBUG = True
